858afcda1ad3d46b6b4add9b61eed95b01f42257
Emil J <112337193+emilj610@users.noreply.github.com>
2024-05-09 17:37:20 +0200

    Update constant.m

--- constant.m
+++ constant.m
@@ -12,24 +12,50 @@ N = 1e2; % number of timesteps
 dt = T/N;
 randn("state",0);
 
-S = zeros(M,1);
-mis_S = zeros(M,1);
+its = 10;
+prices = zeros(its,1);
+values = zeros(its,1);
+values_var = zeros(its,1);
 
 price = BSCH(S0,T,K,r,sigma);
-mis_price = BSCH(S0,T,K,r,mis_sigma);
 
+mis_sigma = 0.45;
+
+%% mis calculated value
+for k = 1:its
+mis_S = zeros(M,1);
+
+mis_price = BSCH(S0,T,K,r,mis_sigma);
+prices(k) = mis_price;
+
+for i = 1:M
+    %generate the Brownian motion
+    dW = sqrt(dt) * randn(N,1);
+    Wt = sum(dW);
+
+    mis_S(i) = S0*exp((r-mis_sigma^2/2)*T+mis_sigma*Wt);
+end
+
+mis_v = exp(-r*T) * max(mis_S-K,0);
+
+values(k) = mean(mis_v);
+values_var(k) = var(mis_v);
+
+mis_sigma = mis_sigma + 0.01;
+end
+
+%% true value
+S = zeros(M,1);
 for i = 1:M
     %generate the Brownian motion
     dW = sqrt(dt) * randn(N,1);
     Wt = sum(dW);
     
     S(i) = S0*exp((r-sigma^2/2)*T+sigma*Wt);
-    mis_S(i) = S0*exp((r-mis_sigma^2/2)*T+mis_sigma*Wt);
 end
 
-v = exp(-r*T) * mean(max(S-K,0));
-mis_v = exp(-r*T) * mean(max(mis_S-K,0));
+v = exp(-r*T) * max(S-K,0);
 
-disp(v)
-disp(mis_v)
 
+true_var = var(v);
+true_value = mean(v);
