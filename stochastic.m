4faaf18d1def9e8202dce7f39020f573fd669838
Sebastian Apelgren <Sebastian Apelgren>
2024-05-09 16:17:21 +0200

    fixade time-dep

--- stochastic.m
+++ stochastic.m
@@ -1,40 +1,78 @@
 clear all, clc, close all
 
-S0 = 3; % initial stock price
+S0 = 50; % initial stock price
 K = 4; % Strike price
-T = 1; % time to expiration
+T = 4; % time to expiration
 
-r = 1; % risk-free rate
-sigma0 = 0.3; % true volatility
-mis_sigma0 = 3; % mis-specified volatility
+%time dependent r and sigma:
+r = @(t) 0.2*t-0.1; % risk-free rate (linjär)
+sigma = @(t) 0.3*(t-(T/2)).^2; % true volatility (parabolisk)
+sigma_mis = @(t) 0.2*t + 0.5; % mis-specified volatility (antar att den är linjär)
 
 M = 1e6; % number Monte Carlo sims
 N = 1e2; % number of timesteps
+
 dt = T/N;
+t_span = 0:dt:T;
 randn("state",0);
 
-S = zeros(M,1);
-mis_S = zeros(M,1);
+t = 0;
+II = zeros(M,1);
+II_mis = zeros(M,1);
 
-%% Det är inte klart!
+S = zeros(M,N+1);
+S_mis = zeros(M,N+1);
 
-sigma = sigma0*ones(M,1); % S(0) for all realizations
-W = zeros(M,1); % W(0) for all realizations
-for j=1:N
-    dW = sqrt(dt)*randn(M,1);
-    S = S + S.*(r*dt+sigma*dW); % processes at next time step
-    W = W + dW; % Brownian paths at next step
-end
+S(:,1) = S0;
+S_mis(:,1) = S0;
 
-for i = 1:M
-    %generate the Brownian motion
-    dW = sqrt(dt) * randn(N,1);
-    Wt = sum(dW);
+for i = 1:N
+
+    t = t + dt;
     
-    S(i) = S0*exp((r-sigma^2/2)*T+sigma*Wt);
-    mis_S(i) = S0*exp((r-mis_sigma^2/2)*T+mis_sigma*Wt);
+    dW = sqrt(dt) * randn(M,1);
+    
+    %calculate integral I 
+    f = @(t) r(t) - 1/2*sigma(t);
+    f_mis = @(t) r(t) - 1/2*sigma_mis(t);
+    I = integral(f,0,t);
+    I_mis = integral(f_mis,0,t);
+
+    %computes integral II with Euler-Maruyama scheme
+    II = sigma(t)*dW;
+    II_mis = sigma_mis(t)*dW;
+
+    %computes stock price
+    S(:,i+1) = S0*exp(I+II); 
+    S_mis(:,i+1) = S0*exp(I_mis + II_mis);
+
 end
 
-v = exp(-r*T) * mean(max(S-K,0));
-mis_v = exp(-r*T) * mean(max(mis_S-K,0));
+III = T/6 * ( r(0) + 4*r(T/2) + r(T) ); %simpsons w/ n = 2
+
+v = exp(-III) * mean(max(S(:,end)-K,0));
+mis_v = exp(-III) * mean(max(S_mis(:,end)-K,0));
+
+disp(v)
+disp(mis_v)
+
+%% plots
+
+hold on
+for i = 1:M/1e5-1
+    plot(t_span,S(i*1e4,:))
+end
+plot2 = plot(t_span, S(end,:), 'r', 'LineWidth', 3); % Highlighted plot
+
+for i = 1:M/1e5-1
+    plot(t_span,S_mis(i*1e4,:))
+end
+plot3 = plot(t_span, S_mis(end,:), 'b', 'LineWidth', 3); % Highlighted plot
+legend([plot2, plot3], {'Exact Stock Price', 'Mispecified Stock Price'}); % Correct usage
+
+title('Simulated stock prices')
+hold off
+
+
+
 
--- time_dep.m
+++ time_dep.m
@@ -1,53 +1,81 @@
 clear all, clc, close all
 
-S0 = 30; % initial stock price
+S0 = 50; % initial stock price
 K = 4; % Strike price
 T = 4; % time to expiration
 
 %time dependent r and sigma:
-r = @(t) 0.2*t + 2; % risk-free rate (linjär)
-sigma = @(t) (t-(T/2)).^2; % true volatility (parabolisk)
-sigma_mis = @(t) 0.2*t + 1; % mis-specified volatility (antar att den är linjär)
+r = @(t) 0.2*t; % risk-free rate (linjär)
+sigma = @(t) 0.3*(t-(T/2)).^2; % true volatility (parabolisk)
+sigma_mis = @(t) 0.2*t + 0.5; % mis-specified volatility (antar att den är linjär)
 
 M = 1e6; % number Monte Carlo sims
 N = 1e2; % number of timesteps
+
 dt = T/N;
 t_span = 0:dt:T;
 randn("state",0);
 
-S = zeros(M,1);
-mis_S = zeros(M,1);
+t = 0;
+II = zeros(M,1);
+II_mis = zeros(M,1);
 
-for i = 1:M
-    %generate the Brownian motion
-    dW = sqrt(dt) * randn(N,1);
+S = zeros(M,N+1);
+S_mis = zeros(M,N+1);
 
-    %calculate integrals for stock price
-    f = @(t) r(t) - 1/2*sigma(t);
-    I = T/6 * ( f(0) + 4*f(T/2) + f(T) ); %simpsons w/ n = 2
+S(:,1) = S0;
+S_mis(:,1) = S0;
 
-    f_mis = @(t) r(t) - 1/2*sigma_mis(t);
-    I_mis = T/6 * ( f_mis(0) + 4*f_mis(T/2) + f_mis(T) ); %simpsons w/ n = 2
+for i = 1:N
 
-    II = sigma(t_span(1:end-1))*dW; %Euler-Maruyama scheme
-    II_mis = sigma_mis(t_span(1:end-1))*dW; %Euler-Maruyama scheme
+    t = t + dt;
     
-    %calculates the stock price at t = T
-    S(i) = S0*exp(I+II); 
-    mis_S(i) = S0*exp(I_mis + II_mis);S = S0*ones(M,1); % S(0) for all realizations
-    W = zeros(M,1); % W(0) for all realizations
-    for j=1:N
-      dW = sqrt(dt)*randn(M,1);
-      S = S + S.*(r*dt+sigma*dW); % processes at next time step
-      W = W + dW; % Brownian paths at next step
-    end
+    dW = sqrt(dt) * randn(M,1);
+    
+    %calculate integral I 
+    f = @(t) r(t) - 1/2*sigma(t);
+    f_mis = @(t) r(t) - 1/2*sigma_mis(t);
+    I = integral(f,0,t);
+    I_mis = integral(f_mis,0,t);
+
+    %computes integral II with Euler-Maruyama scheme
+    II = sigma(t)*dW;
+    II_mis = sigma_mis(t)*dW;
+
+    %computes stock price
+    S(:,i+1) = S0*exp(I+II); 
+    S_mis(:,i+1) = S0*exp(I_mis + II_mis);
+
 end
 
 III = T/6 * ( r(0) + 4*r(T/2) + r(T) ); %simpsons w/ n = 2
 
-v = exp(-III) * mean(max(S-K,0));
-mis_v = exp(-III) * mean(max(mis_S-K,0));
+v = exp(-III) * mean(max(S(:,end)-K,0));
+mis_v = exp(-III) * mean(max(S_mis(:,end)-K,0));
 
 disp(v)
 disp(mis_v)
+
+%% plots
+
+hold on
+for i = 1:M/1e5-1
+    plot(t_span,S(i*1e4,:))
+end
+plot2 = plot(t_span, S(end,:), 'r', 'LineWidth', 3); % Highlighted plot
+
+for i = 1:M/1e5-1
+    plot(t_span,S_mis(i*1e4,:))
+end
+plot3 = plot(t_span, S_mis(end,:), 'b', 'LineWidth', 3); % Highlighted plot
+legend([plot2, plot3], {'Exact Stock Price', 'Mispecified Stock Price'}); % Correct usage
+
+title('Simulated stock prices')
+hold off
+
+figure()
+hold on
+plot(t_span,sigma(t_span),'r')
+plot(t_span,sigma_mis(t_span),'b')
+
 
